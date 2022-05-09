5bc15442540abd36e6adf515bf64f0df997d6cbb
henriqueacker <75759842+henriqueacker@users.noreply.github.com>
2022-05-09 19:49:09 -0300

    Alteração

---
+++ src/controllers/JogadorController.php
@@ -0,0 +1,15 @@
+<?php
+
+use core\Controller;
+
+class JogadorController extends Controller{
+
+    public function add(){
+        $this->render('404');
+    }
+    public function cadastrarJogador(){}
+    public function listarJogador(){}
+    public function deletarJogador(){}
+    public function editarJogador(){}
+    
+}
