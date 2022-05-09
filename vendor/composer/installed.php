f1f1732ba927952b7dd3a237be41872c5d9572e1
Henrique Bastos <75759842+henriqueacker@users.noreply.github.com>
2022-05-09 20:44:21 -0300

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '378396dfcb932b9b6654b08e4edd8dcb2b620d56',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '378396dfcb932b9b6654b08e4edd8dcb2b620d56',
+            'dev_requirement' => false,
+        ),
+        'clancats/hydrahon' => array(
+            'pretty_version' => 'v1.1.12',
+            'version' => '1.1.12.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../clancats/hydrahon',
+            'aliases' => array(),
+            'reference' => 'cc2fc3fd2ea082a2d531a751da49cc6a413270cd',
+            'dev_requirement' => false,
+        ),
+    ),
+);
